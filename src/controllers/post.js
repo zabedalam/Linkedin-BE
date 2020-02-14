2ee3fbadc0ea13ebcd17c5956a0a09306c599c8e
zabed <zabedalam777@gmail.com>
2020-02-14 14:35:40 +0100

    get is working

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,19 +1,27 @@
 const Post=require("../models/post")
 exports.getPost=(req,res)=>{
     // res.send("Hello Me Here")
-    res.json({
-        post:[
-            {title:"first post"},
-            {title:"second post"}
-        ]
+    // res.json({
+    //     post:[
+    //         {title:"first post"},
+    //         {title:"second post"}
+    //     ]
+    // })
+
+    const posts=Post.find().select("_id title body")
+    .then(posts=>{
+        res.status(200).json({
+            posts:posts
+        })
     })
+    .catch(err=>console.log(err))
 }
 
 exports.createPost=(req,res)=>{
     const post =new Post(req.body)
     // console.log("CREATING POST: ",req.body)
     //saving the post always requied error handling
-    
+
     // post.save((err,result)=>{
     //     if(err){
     //        return res.status(400).json({
